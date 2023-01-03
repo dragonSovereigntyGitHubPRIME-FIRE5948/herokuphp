8223f280746bb365895ce4daaab6289c7741fad6
dragonSovereigntyGitHubPRIME-FIRE5948 <21040247@myrp.edu.sg>
2023-01-03 17:34:31 +0800

    commit message goes here

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/heroku',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1940aef294b7439afa8eef030d58e639f69c8a96',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/heroku' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1940aef294b7439afa8eef030d58e639f69c8a96',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
