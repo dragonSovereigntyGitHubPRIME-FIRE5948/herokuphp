0ddc2542d9c4db89c240884e32886f5b54676166
dragonSovereigntyGitHubPRIME-FIRE5948 <21040247@myrp.edu.sg>
2023-01-03 23:34:47 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,12 +1,15 @@
-<!-- test -->
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks-mode -->
 <!DOCTYPE html>
-<!-- test -->
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
-    <!-- test -->
+    <!-- fix : Document doesn't have a <title> element -->
     <title>Activity 3</title>
+    <!-- fix : Document does not have a meta description -->
     <meta name="description" content="This is Activity 3">
 <head>
-    <!-- test -->
+    <!-- fix : Tap targets are not sized appropriately -->
+    <!-- fix : Document doesn't use legible font sizes -->
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
