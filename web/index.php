7359af652a4ec3d892fba31a1c755bafd56baa7e
dragonSovereigntyGitHubPRIME-FIRE5948 <21040247@myrp.edu.sg>
2023-01-03 23:26:54 +0800

    commit message goes here

--- web/index.php
+++ web/index.php
@@ -1,3 +1,19 @@
+<!-- test -->
+<!DOCTYPE html>
+<!-- test -->
+<html lang="en">
+    <!-- test -->
+    <title>Activity 3</title>
+    <meta name="description" content="This is Activity 3">
+<head>
+    <!-- test -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  Hello World! 2
+</body> 
+</html>
+
 <?php
 
 echo "Hello World!";
