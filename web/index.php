b53fbe77913a70ccafd879a6f16ada146f2932fe
dragonSovereigntyGitHubPRIME-FIRE5948 <21040247@myrp.edu.sg>
2023-01-04 10:34:08 +0800

    commit

--- web/index.php
+++ web/index.php
@@ -16,7 +16,7 @@
   <body>
     <!-- Hello World -->
 
-    <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg">
+    <img alt="test" loading="lazy" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg">
 
     <script>
 
